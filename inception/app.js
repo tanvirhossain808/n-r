af4933b60d78e2073cf1e77f84a30370efbf7bf0
Md. Tanvir Hossain <151984579+tanvirhossain808@users.noreply.github.com>
2024-08-09 01:28:48 +0600

    add inception code

---
+++ inception/app.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div", {}, [React.createElement("h1", {}, "This is h1 tag")])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
+console.log(ReactDOM);
